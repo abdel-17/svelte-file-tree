9d5f2417a3114630c47d82190dea5ab7896bf8f3
Abdelrahman Bahaa <abdelrahmanb015@gmail.com>
2024-10-15 21:52:36 +0300

    setup prettier for each project separately

--- sites/preview/src/routes/+page.svelte
+++ sites/preview/src/routes/+page.svelte
@@ -8,13 +8,13 @@
 	});
 </script>
 
-<TreeView {tree} class="border m-8">
+<TreeView {tree} class="m-8 border">
 	{#snippet children(item)}
 		<TreeItem
 			{item}
 			data-leaf={item.children.length === 0 ? "" : undefined}
 			style="--depth: {item.depth}"
-			class="group flex gap-2 p-2 ms-[calc(var(--spacing-4)*var(--depth))] aria-selected:bg-blue-100 aria-selected:text-blue-900 focus-visible:outline focus-visible:outline-2 focus-visible:outline-blue-600"
+			class="group ms-[calc(var(--spacing-4)*var(--depth))] flex gap-2 p-2 focus-visible:outline focus-visible:outline-2 focus-visible:outline-blue-600 aria-selected:bg-blue-100 aria-selected:text-blue-900"
 		>
 			<ChevronDown
 				class="transition-transform duration-300 group-aria-expanded:rotate-180 group-data-leaf:invisible"
