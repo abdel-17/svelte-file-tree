7a41b6d72723baf01ab5813239ab004e861e024d
Abdelrahman Bahaa <abdelrahmanb015@gmail.com>
2024-10-16 21:58:59 +0300

    use adapter static

--- sites/preview/svelte.config.js
+++ sites/preview/svelte.config.js
@@ -1,7 +1,7 @@
-import adapter from "@sveltejs/adapter-auto";
+import adapter from "@sveltejs/adapter-static";
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
-/** @type {import('@sveltejs/kit').Config} */
+/** @type {import("@sveltejs/kit").Config} */
 export default {
 	preprocess: vitePreprocess(),
 	kit: {
